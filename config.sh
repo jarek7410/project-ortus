83b215435cb2a23913ce80c39b4db4eedab5b481
jarek <jarekomsta@wp.pl>
2021-12-03 12:00:40 +0100

    config inicial

---
+++ config.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+git config --global user.name jarek7410
+git config --global user.email jrk@student.agh.edu.pl 
+git config --global push.default simple
+git config --global credential.helper "cache --timeout=3600"
+#  git clone https://gitlab.kt.agh.edu.pl/TI_2021_inz_1/pi_2021.git
+#  echo some tape of error
+echo end of git configuration
+cp .vimrc ~/.vimrc
+echo end of vim configuration
+echo end of configuration.
+
+
